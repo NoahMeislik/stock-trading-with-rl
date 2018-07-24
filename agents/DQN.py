3f013977ebe6fd93676f0046df1f06716d97ba12
Unknown <noah.meislik@gmail.com>
2018-07-24 00:24:35 -0700

    Fixed tensorboard Storage Issue

--- agents/DQN.py
+++ agents/DQN.py
@@ -120,8 +120,8 @@ class Agent(Agent):
 
             target_f = self.sess.run(self.logits, feed_dict={self.X_input: state})
             target_f[0][action] = target
-            _, c, s = self.sess.run([self.train_op, self.loss_op, self.summ], feed_dict={self.X_input: state, self.Y_input: target_f})
-            self.writer.add_summary(s, (episode + 1) * time)
+            _, c = self.sess.run([self.train_op, self.loss_op], feed_dict={self.X_input: state, self.Y_input: target_f}) # Add self.summ into the sess.run for tensorboard
+            # self.writer.add_summary(s, (episode + 1) * time)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay 
